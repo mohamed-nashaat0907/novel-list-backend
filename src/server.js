55cee0104b2ca9d2672e0b837c940f790356283e
mohamed-nashaat0907 <mohamed-nashaat0907@gmail.com>
2026-01-18 16:16:04 +0200

    second edited backend

--- src/server.js
+++ src/server.js
@@ -100,10 +100,16 @@ io.on("connection", async (socket) => {
 
 // --- Server start ---
 const PORT = process.env.PORT || 3000;
-const Server = server.listen(PORT, () => {
+
+const Server = server.listen(PORT, '0.0.0.0', () => {
   connectDB();
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
+// const Server = server.listen(PORT, () => {
+//   connectDB();
+//   console.log(`🚀 Server running on port ${PORT}`);
+// });
 
 // --- Global error handling ---
 process.on("unhandledRejection", (err) => {
